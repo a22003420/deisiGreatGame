714eff945b3caff34f7b1469e2c8a0f60cdc702a
João Matos <jpcamatos31@gmail.com>
2021-10-30 19:15:34 +0100

    Initial commit

---
+++ src/pt/ulusofona/lp2/deisiGreatGame/Programmer.java
@@ -0,0 +1,26 @@
+package pt.ulusofona.lp2.deisiGreatGame;
+
+public class Programmer {
+
+    int getId(){
+        return 0;
+    }
+
+    String getName(){
+        return "";
+
+    }
+
+    //ProgrammerColor getColor(){
+
+
+    //}
+
+
+
+    public String toString(){
+        return "";
+    }
+
+
+}
