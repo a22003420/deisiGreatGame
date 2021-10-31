e8a039de416ebd83e21e0f9e51fa8f7e7b6c7a58
João Matos <jpcamatos31@gmail.com>
2021-10-31 20:25:44 +0000

    Tests_1stVersion_31102021

---
+++ src/pt/ulusofona/lp2/deisiGreatGame/TestGameManager.java
@@ -0,0 +1,39 @@
+package pt.ulusofona.lp2.deisiGreatGame;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+
+
+public class TestGameManager {
+    @Test
+    public void test01(){
+
+
+
+
+    }
+
+    @Test
+    public void test02(){
+
+
+
+
+    }
+
+    /*@Test
+    public void test03Depositar(){
+
+        ContaBancaria conta = new ContaBancaria (20);
+        conta.depositar(100);
+        long saldoEsperado = 120;
+        long saldoObtido = conta.getSaldo();
+        assertEquals(saldoEsperado, saldoObtido);
+
+
+    }*/
+
+
+
+}
