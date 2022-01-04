0ae7a72bd3e4be9ebfd4075b5d492c86e96cc2c2
Rui Fazendeiro <RuiFazendeiro@sapo.pt>
2022-01-04 22:34:44 +0000

    Included Functions.kt

---
+++ src/pt/ulusofona/lp2/deisiGreatGame/Functions.kt
@@ -0,0 +1,12 @@
+package pt.ulusofona.lp2.deisiGreatGame
+
+enum class CommandType{
+    GET,
+    POST
+}
+
+/*
+val routerFn = router()
+val commandGetFn = routerFn.invoke(CommandType.GET)
+val result = commandGetFn.invoke(manager, listOf("PLAYER", "Joshua"))
+ */
