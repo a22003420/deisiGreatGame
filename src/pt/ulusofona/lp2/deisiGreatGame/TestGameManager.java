d5ca5316e92edaf1f1bc00770476c673f1e60556
Rui Fazendeiro <RuiFazendeiro@sapo.pt>
2022-01-08 23:05:14 +0000

    Fixes

--- src/pt/ulusofona/lp2/deisiGreatGame/TestGameManager.java
+++ src/pt/ulusofona/lp2/deisiGreatGame/TestGameManager.java
@@ -80,9 +80,9 @@ public class TestGameManager{
         assertEquals(CURRENT_PLAYER_POSITION_AFTER_MOVE, 3, (int) currentPlayer.currentPosition());
         reactToTitle(game);
 
-        String result = postAbyss(game, 1, 1);
-        String xxx = "sss";
-        //assertEquals("2:3\n2:1", getMostUsedPositions(game.getProgrammers(true), 2));
+        //String result = postAbyss(game, 1, 1);
+        //String xxx = "sss";
+        assertEquals("3:2\n1:2", getMostUsedPositions(game.getProgrammers(true), 2));
     }
 
     /*
